359299cb117b762475d278f21684b62e96f5e067
Adoox <adnankubat40@gmail.com>
2022-11-26 23:32:30 +0100

    implemented video card with real data from sanity backend

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -13,6 +13,21 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
+  const [isHover, setisHover] = useState(false);
+  const [playing, setPlaying] = useState(false);
+  const [isVideoMuted, setisVideoMuted] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef?.current?.pause();
+      setPlaying(false);
+    } else {
+      videoRef?.current?.play();
+      setPlaying(true);
+    }
+  };
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
@@ -33,12 +48,57 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           </div>
           <div>
             <Link href="/">
-              <div>
-                <p>{post.postedBy.userName}</p>
-                <GoVerified className="text-blue-400 text-md" />
+              <div className="flex items-center gap-2">
+                <p className="flex items-center gap-2 md:text-md font-bold text-primary">
+                  {post.postedBy.userName}{" "}
+                  <GoVerified className="text-blue-400 text-md" />
+                  <p className="font-medium text-xs text-gray-500 capitalize hidden md:block">
+                    {post.postedBy.userName}
+                  </p>
+                </p>
               </div>
             </Link>
           </div>
+        </div>
+      </div>
+
+      <div className="lg:ml-20 flex gap-4 relative">
+        <div
+          onMouseEnter={() => setisHover(true)}
+          onMouseLeave={() => setisHover(false)}
+          className="rounded-3xl"
+        >
+          <Link href="/">
+            <video
+              ref={videoRef}
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              loop
+              src={post.video.asset.url}
+            ></video>
+          </Link>
+          {isHover && (
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
+              {playing ? (
+                <button onClick={onVideoPress}>
+                  <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={onVideoPress}>
+                  <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+
+              {isVideoMuted ? (
+                <button onClick={() => setisVideoMuted(false)}>
+                  <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
+                </button>
+              ) : (
+                <button onClick={() => setisVideoMuted(true)}>
+                  <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
+                </button>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
