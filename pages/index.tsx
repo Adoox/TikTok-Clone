2e699bcee57b1402c9724689419159da45681912
Adoox <adnankubat40@gmail.com>
2022-11-26 23:02:44 +0100

    connected the app with sanity backend and make a get request for fetching data from sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,36 @@ import Image from "next/image";
 
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
+import NoResults from "../components/NoResults";
+
+interface IProps {
+  videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
   return (
-    <div className="text-xl font-bold italic text-red-700">
-      <h2>VisiTok</h2>
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video: Video) => <VideoCard post={video} key={video._id} />)
+      ) : (
+        <NoResults text={"Nema nista na backendu"} />
+      )}
     </div>
   );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data,
+    },
+  };
 };
 
 export default Home;
